09f89acd66942a87e4ad57e89e5d8315347a9b51
gbrieal <gabrielgalvaoo@gmail.com>
2022-04-27 01:11:58 -0300

    inserindo o código

---
+++ script.js
@@ -0,0 +1,68 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 25)
+        } else {
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random()* 6000;
+
+    console.log(randomTime);
+
+    cactus.classList.add('cactus');
+    cactus.style.left = cactusPosition + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class = "game-over">FIM DE JOGO</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
